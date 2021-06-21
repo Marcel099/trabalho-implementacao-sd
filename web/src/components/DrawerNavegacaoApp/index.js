4aa3b5320b7a2692164792e89963c54483b13260
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2021-06-21 00:24:31 -0300

    Navegação principal do app elaborada

---
+++ web/src/components/DrawerNavegacaoApp/index.js
@@ -0,0 +1,98 @@
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom'
+import clsx from 'clsx';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import Commute from '@material-ui/icons/Commute';
+import AddLocation from '@material-ui/icons/AddLocation';
+import Assessment from '@material-ui/icons/Assessment';
+import Explore from '@material-ui/icons/Explore';
+
+import { ContextoDrawerNavegacao } from '../../contexts/ContextoDrawerNavegacao';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
+export function DrawerNavegacaoApp() {
+  const {
+    isActive,
+    anchor,
+    toggleDrawer,
+  } = useContext(ContextoDrawerNavegacao);
+
+  const classes = useStyles()
+
+  const history = useHistory();
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <ListItem button onClick={() => history.push('/veiculo')}>
+            <ListItemIcon>
+              <Commute />
+            </ListItemIcon>
+            <ListItemText primary={'Manutenção de Veículos'} />
+        </ListItem>
+
+        <ListItem button onClick={() => history.push('/localizacao/new')}>
+          <ListItemIcon>
+            <AddLocation />
+          </ListItemIcon>
+          <ListItemText primary={'Informar Localização'} />
+        </ListItem>
+
+        <ListItem button onClick={() => history.push('/monitoramento-veiculos')}>
+          <ListItemIcon>
+            <Assessment />
+          </ListItemIcon>
+          <ListItemText primary={'Monitoramento de Veículos'} />
+        </ListItem>
+
+        <ListItem button onClick={() => history.push('/veiculo/simulador-deslocamento')}>
+          <ListItemIcon>
+            <Explore />
+          </ListItemIcon>
+          <ListItemText primary={'Simulador de Deslocamento'} />
+        </ListItem>
+      </List>
+    </div>
+  );
+
+  return (
+    <aside>
+      <Drawer anchor={anchor} open={isActive} onClose={toggleDrawer(false)}>
+        {list(anchor)}
+      </Drawer>
+    </aside>
+  )
+}
+
+/*
+Organização alternativa pra pedir ao Rebonatto:
+
+
+-- Manutenção de veículos
+---- Informar localização atual de um veículo selecionado
+---- Simulador de Deslocamento de um veículo selecionado
+-- Simulador de Deslocamento
+*/
--- web/src/routes.js
+++ web/src/routes.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+
+import { DrawerNavegacaoProvider } from './contexts/ContextoDrawerNavegacao';
+
+import { DrawerNavegacaoApp } from './components/DrawerNavegacaoApp'
+import { CabecalhoApp } from './components/CabecalhoApp'
+
+
 import { OperadorLogon } from './pages/OperadorLogon'
 
 import { ListVeiculo } from './pages/veiculo/ListVeiculo'
@@ -10,14 +17,19 @@ import { EditVeiculo } from './pages/veiculo/EditVeiculo'
 
 import { NewLocalizacao } from './pages/localizacao/NewLocalizacao'
 
-import { Movimentacao } from './pages/MonitoramentoVeiculos/Movimentacao'
-import { UltimaLocalizacaoVeiculos } from './pages/MonitoramentoVeiculos/UltimaLocalizacaoVeiculos'
+import { MonitoramentoVeiculos } from './pages/MonitoramentoVeiculos'
 
 import { Simulador } from './pages/SimuladorDeslocamento'
 
 export function Routes(){
   return (
     <BrowserRouter>
+      <Switch>
+        <DrawerNavegacaoProvider>
+          <CabecalhoApp />
+          <DrawerNavegacaoApp />
+        </DrawerNavegacaoProvider>
+      </Switch>
       <Switch>
         <Route path="/" exact component={OperadorLogon} />
 
@@ -27,10 +39,9 @@ export function Routes(){
 
         <Route path="/localizacao/new" component={NewLocalizacao} />
 
-        <Route path="/monitoramento-veiculos/ultima-localizacao-veiculos" component={UltimaLocalizacaoVeiculos} />
-        <Route path="/monitoramento-veiculos/movimentacao" component={Movimentacao} />
+        <Route path="/monitoramento-veiculos" component={MonitoramentoVeiculos} />
 
-        <Route path="/veiculo/:codigo_veiculo/simulador-deslocamento" component={Simulador} />
+        <Route path="/veiculo/simulador-deslocamento" component={Simulador} />
       </Switch>
     </BrowserRouter>
   )
