5dd7479f137cbe3500c8c11d71c91fcd34f5ae2b
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2021-06-21 11:34:52 -0300

    Pequeno ajuste de acesso ao DrawerContext

--- web/src/components/LayoutBase/index.js
+++ web/src/components/LayoutBase/index.js
@@ -2,19 +2,21 @@ import React from 'react'
 
 import { DrawerNavegacaoProvider } from '../../contexts/ContextoDrawerNavegacao';
 
-import { DrawerNavegacaoApp } from '../DrawerNavegacaoApp'
 import { CabecalhoApp } from '../CabecalhoApp'
+import { DrawerNavegacaoApp } from '../DrawerNavegacaoApp'
 
 import { ContainerApp } from '../ContainerApp'
 
 export function LayoutBase({ children }) {
   return (
-    <DrawerNavegacaoProvider>
-      <CabecalhoApp />
-      <DrawerNavegacaoApp />
+    <>
+      <DrawerNavegacaoProvider>
+        <CabecalhoApp />
+        <DrawerNavegacaoApp />
+      </DrawerNavegacaoProvider>
       <ContainerApp>
         {children}
       </ContainerApp>
-    </DrawerNavegacaoProvider>
+    </>
   )
 }
