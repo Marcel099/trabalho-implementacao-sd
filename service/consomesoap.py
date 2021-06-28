cf1898588ceb4a40bba7cb1f0ee2eb7fa7bbbdb4
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2021-06-28 14:24:16 -0300

    Corr. de vários bugs no retorno das rotas SOAP

--- service/consomesoap.py
+++ service/consomesoap.py
@@ -1,5 +1,20 @@
 from zeep import *
 
 client = Client('http://localhost:8000/?wsdl')
-response = client.service.adiciona('BRA0S19', 3, 'Bicicleta', True, 3)
+# response = client.service.adiciona('BRA0S19', 3, 'Bicicleta', True, 3)
+# print(response)
+#
+# response = client.service.altera(10, 'BRA0S19', 3, 'Bicicleta Alterada', True, 3)
+# print(response)
+
+# response = client.service.excluir(10)
+# print(response)
+
+# response = client.service.consulta(5)
+# print(response)
+
+response = client.service.listainstituicao(2)
 print(response)
+
+# response2 = client.service.listatipo(2, 5)
+# print(response2)
--- service/soapAPI.py
+++ service/soapAPI.py
@@ -1,74 +1,110 @@
-from spyne import Application, rpc, ServiceBase, Unicode, AnyXml
+from spyne import Application, rpc, ServiceBase, Unicode, Boolean, Integer, ComplexModel, Array
 from spyne.protocol.soap import Soap12
 from spyne.server.wsgi import WsgiApplication
 from wsgiref.simple_server import make_server
-import xml.etree.ElementTree as ET
+# import xml.etree.ElementTree as ET
 import psycopg2
 import logging
 
 HOST='localhost'
 DATABASE='postgres'
+# DATABASE='db_esp'
 USER='postgres'
 PASSWORD='rafa'
+# PASSWORD='Breaker#2030'
+
+# SQL_DB_URI = 'postgresql://postgres:Breaker#2030@localhost:5432/db_esp'
 
 conn = psycopg2.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
 
-def create_xml(res_list):
-        # we make root element
-        usrconfig = ET.Element("usrconfig")
-  
-        # create sub element
-        usrconfig = ET.SubElement(usrconfig, "usrconfig")
-  
-        # insert list element into sub elements
-        for user in range(len(res_list)):
-  
-                usr = ET.SubElement(usrconfig, "usr")
-                usr.text = str(res_list[user])
-  
-        tree = ET.ElementTree(usrconfig)
-  
-        # write the tree into an XML file
-        tree.write("Output.xml", encoding ='utf-8', xml_declaration = True)
-        return tree
+class Vehicle(ComplexModel):
+    codigo = Integer
+    placa = Unicode
+    tipo = Integer
+    descricao = Unicode
+    visiveltodos = Boolean
+    instituicao = Integer
 
+# def create_xml(res_list):
+#         # we make root element
+#         usrconfig = ET.Element("usrconfig")
+#
+#         # create sub element
+#         usrconfig = ET.SubElement(usrconfig, "usrconfig")
+#
+#         # insert list element into sub elements
+#         for user in range(len(res_list)):
+#
+#                 usr = ET.SubElement(usrconfig, "usr")
+#                 usr.text = str(res_list[user])
+#
+#         tree = ET.ElementTree(usrconfig)
+#
+#         # write the tree into an XML file
+#         tree.write("Output.xml", encoding ='utf-8', xml_declaration = True)
+#         return tree
+
+def convertVehicleListToVehicleObject(vehicle_list):
+    return Vehicle(
+        codigo=vehicle_list[0],
+        placa=vehicle_list[1],
+        tipo=vehicle_list[2],
+        descricao=vehicle_list[3],
+        visiveltodos=vehicle_list[4],
+        instituicao=vehicle_list[5]
+    )
 
 class crudsoap(ServiceBase):
-    @rpc(Unicode, Unicode, Unicode, Unicode, Unicode, _returns=Unicode)
+    @rpc(Unicode, Integer, Unicode, Boolean, Integer, _returns=Unicode)
     def adiciona(self, placa, tipo, desc, vt, inst):
-        sql = ("INSERT INTO veiculos(placa, tipo, descricao, visiveltodos, instituicao) VALUES (%s, %s ,%s, %s, %s)" % (placa, tipo, desc, vt, inst))
-        
+        sql = f"INSERT INTO veiculos(placa, tipo, descricao, visiveltodos, instituicao) VALUES " +\
+              f"('{placa}', {tipo} ,'{desc}', {vt}, {inst})"
+
         cur = conn.cursor()
         cur.execute(sql)
         cur.close()
-        
-        return ("funcao adiciona")
 
-    @rpc(Unicode, _returns=Unicode)
-    def altera(self, idveiculo):
-        sql = ('UPDATE veiculo SET %s=%s WHERE codigo = ' % idveiculo)
+        conn.commit()
+
         
+        return "funcao adiciona"
+
+    @rpc(Integer, Unicode, Integer, Unicode, Boolean, Integer, _returns=Unicode)
+    def altera(self, idveiculo, placa, tipo, desc, vt, inst):
+      
+        sql = f"UPDATE veiculos " +\
+              f"SET placa = \'{placa}\', " +\
+              f"tipo = {tipo}, " +\
+              f"descricao = \'{desc}\', " +\
+              f"visiveltodos = {vt}, " +\
+              f"instituicao = {inst} " +\
+              f"WHERE codigo = {idveiculo}"
+
         cur = conn.cursor()
         cur.execute(sql)
-        res = cur.fetchone()
+        # cur.fetchone()  # res =
         cur.close()
-        
-        return "deletado"
 
-    @rpc(Unicode, _returns=Unicode)
+        conn.commit()
+        
+        return "alterado"
+
+    @rpc(Integer, _returns=Unicode)
     def excluir(self, idveiculo):
         sql = f'DELETE FROM veiculos WHERE codigo={idveiculo}'
         
         cur = conn.cursor()
         cur.execute(sql)
-        res_list = cur.fetchone()
+        # cur.fetchone()  # res =
         cur.close()
-        
-        print(create_xml(res_list))
-        print(res_list)
+
+        conn.commit()
+
+        # print(create_xml(res_list))
+        # print(res_list)
         return 'deletado'
 
-    @rpc(Unicode, _returns=Unicode)
+    @rpc(Integer, _returns=Vehicle)
     def consulta(self, idveiculo):
         sql = f'SELECT * FROM veiculos WHERE codigo={idveiculo}'
         
@@ -76,12 +112,13 @@ class crudsoap(ServiceBase):
         cur.execute(sql)
         res_list = cur.fetchone()
         cur.close()
-        
-        print(create_xml(res_list))
-        print(res_list)
-        return "OK"
 
-    @rpc(Unicode, _returns=Unicode)
+        veiculo = convertVehicleListToVehicleObject(res_list)
+        
+
+        return veiculo
+
+    @rpc(Integer, _returns=Array(Vehicle))
     def listainstituicao(self, idinstituicao):
         sql = f'SELECT * FROM veiculos WHERE instituicao={idinstituicao}'
         
@@ -89,12 +126,12 @@ class crudsoap(ServiceBase):
         cur.execute(sql)
         res_list = cur.fetchall()
         cur.close()
-        
-        print(create_xml(res_list))
-        print(res_list)
-        return "OK"
 
-    @rpc(Unicode, Unicode, _returns=Unicode)
+        parsedVehicleList = map(lambda vehicle_list: convertVehicleListToVehicleObject(vehicle_list), res_list)
+
+        return parsedVehicleList
+
+    @rpc(Integer, Integer, _returns=Array(Vehicle))
     def listatipo(self, idinstituicao, tipoVeiculo):
         sql = f'SELECT * FROM veiculos WHERE instituicao={idinstituicao} and tipo={tipoVeiculo}'
         
@@ -102,19 +139,20 @@ class crudsoap(ServiceBase):
         cur.execute(sql)
         res_list = cur.fetchall()
         cur.close()
-        
-        print(create_xml(res_list))
-        print(res_list)
-        return "OK"
 
 
-application = Application([crudsoap], 'spyne.examples.hello.soap',
-                          in_protocol=Soap12(validator='lxml'),
-                          out_protocol=Soap12(validator='lxml'))
+        parsedVehicleList = map(lambda vehicle_list: convertVehicleListToVehicleObject(vehicle_list), res_list)
 
-wsgi_application = WsgiApplication(application)
+        return parsedVehicleList
+
 
 if __name__ == '__main__':
+    application = Application([crudsoap], 'spyne.examples.hello.soap',
+                              in_protocol=Soap12(validator='lxml'),
+                              out_protocol=Soap12(validator='lxml'))
+
+    wsgi_application = WsgiApplication(application)
+
     print ("listening to http://127.0.0.1:8000")
     print ("wsdl is at: http://localhost:8000/?wsdl")
 
