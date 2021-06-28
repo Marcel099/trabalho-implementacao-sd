e498d14ad443a8eee0df905c9392234238e4d21a
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2021-06-28 12:08:02 -0300

    Mapa gerado na página Monit. de Veí.

--- web/src/components/Movimentacao/index.js
+++ web/src/components/Movimentacao/index.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+// import { api_rest } from '../../services/api_rest'
+
+import './styles.css'
 
 export function Movimentacao() {
+
+  const [initialPosition, setInitialPosition] = useState([
+    -28.25346343191986,
+    -52.39588558673859
+  ])
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+
+      setInitialPosition([latitude, longitude])
+    })
+  }, [])
+
+  useEffect(() => {
+    api_rest
+  }, [])
+
   return (
-    <h2>Movimentação de um veículo em um período</h2>
+    <div id="container-movimentacao">
+      <h2>Movimentação de um veículo em um período</h2>
+      <Map center={initialPosition} zoom={15}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* <Marker position={selectedPosition}/> */}
+        <Marker position={initialPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    </div>
   )
 }
