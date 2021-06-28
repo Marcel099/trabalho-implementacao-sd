54dbc40c6539e973593127380d1d44f7f2501dab
Ale1702 <53585174+Ale1702@users.noreply.github.com>
2021-06-28 01:39:57 -0300

    Submissão falsa do cadastro de localização

--- web/src/pages/localizacao/NewLocalizacao/index.js
+++ web/src/pages/localizacao/NewLocalizacao/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { 
   TextField,
   Select,
@@ -8,30 +8,54 @@ import {
 } from '@material-ui/core';
 
 //import { api_rest } from '../../../services/api_rest';
+//import { api_soap } from '../../../services/api_soap';
 
 export function NewLocalizacao() {
-  //const data = {
-    //veiculo,
-    //latitude, 
-    //longitude 
-  //}
-  // console.log('data', data)
-  //const response = api_rest.post('points', data)
-  const listaVeiculos=[
-    {
-      nome: 'Ambulancia samu carro de policia',
-      id: 1,
-    },
-    {
-      nome: 'carro',
-      id: 2,
-    },
-  ]
+  
+  const [listaVeiculos, setListaVeiculos] = useState([])
 
-  function handleSubmit(event){
-    event.preventDefault()
-    console.log("Enviou!")
+  const [idVeiculo, setIdVeiculo] = useState(null)
+
+  const [latitude, setLatitude] = useState('')
+  const [longitude, setLongitude] = useState('')
+
+  
+
+  useEffect(function(){
+    function buscarVeiculos(){
+    //let listaVeiculosRecebidos = await api_soap.get('listVeiculos')
     
+    const listaVeiculosRecebidos=[
+        {
+          nome: 'Ambulancia samu carro de policia',
+          id: 1,
+        },
+        {
+          nome: 'carro',
+          id: 2,
+        },
+      ]
+
+      setListaVeiculos(listaVeiculosRecebidos)
+    }
+
+    buscarVeiculos()
+    
+  }, [])
+  
+
+  async function handleSubmit(event){
+    event.preventDefault()
+
+    const data= {
+      latitude: latitude,
+      longitude: longitude,
+      idVeiculo: idVeiculo,
+    }
+
+    console.log(data)
+    
+    //await api_rest.post('/posicao/insert', data)
   }
 
   return (
@@ -39,20 +63,44 @@ export function NewLocalizacao() {
       <form onSubmit={handleSubmit}>
         <FormControl>
           <InputLabel htmlFor="cadastro-localizacao-veiculo">Veículo</InputLabel>
-          <Select native inputProps={{
-            name: 'veiculo',
-            id: 'cadastro-localizacao-veiculo'
-          }}>
+          <Select 
+            native
+            onChange={event => setIdVeiculo(Number(event.target.value))} 
+            inputProps={
+            {
+              name: 'veiculo',
+              id: 'cadastro-localizacao-veiculo'
+            }}
+          >
             {listaVeiculos.map(function(veiculo){
-              return(<option key={veiculo.id}>{veiculo.nome}</option>)
+              return(
+                <option key={veiculo.id} value={veiculo.id}>
+                  {veiculo.nome}
+                </option>
+              )
             })}
           </Select>
         </FormControl>
         <br />
-        <TextField label="latitude" />
-        <TextField label="longitude" />
+        <TextField 
+          type="number"
+          label="latitude" 
+          onChange={event => setLatitude(event.target.value)}
+          value={latitude}
+          style={{paddingRight: "16px"}}
+        />
+        <TextField
+          type="number"
+          label="longitude"
+          onChange={event => setLongitude(event.target.value)}
+          value={longitude} 
+        />
         <br/><br/>
-        <Button variant="contained" color="primary">
+        <Button 
+          type="submit"
+          variant="contained" 
+          color="primary"
+        >
           Atualizar Localização
         </Button>
       </form>
